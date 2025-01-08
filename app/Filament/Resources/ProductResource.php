2663221877c227535e094a114405190bfbbdf0f0
to-sanchez <157402651+to-sanchez@users.noreply.github.com>
2025-01-08 14:13:23 -0400

    Implement Product CRUD

---
+++ app/Filament/Resources/ProductResource.php
@@ -0,0 +1,178 @@
+<?php
+
+namespace App\Filament\Resources;
+
+use App\Enums\ProductStatusEnum;
+use App\Enums\RolesEnum;
+use App\Filament\Resources\ProductResource\Pages;
+use App\Filament\Resources\ProductResource\RelationManagers;
+use App\Models\Product;
+use App\Models\User;
+use Filament\Facades\Filament;
+use Filament\Forms;
+use Filament\Forms\Form;
+use Filament\Forms\Get;
+use Filament\Forms\Set;
+use Filament\Resources\Resource;
+use Filament\Tables;
+use Filament\Tables\Table;
+use Illuminate\Database\Eloquent\Builder;
+use Illuminate\Database\Eloquent\SoftDeletingScope;
+use Illuminate\Support\Str;
+
+class ProductResource extends Resource
+{
+    protected static ?string $model = Product::class;
+
+    protected static ?string $navigationIcon = 'heroicon-o-rectangle-stack';
+
+    public static function form(Form $form): Form
+    {
+        return $form
+            ->schema([
+                Forms\Components\Grid::make()
+                    ->schema([
+                        Forms\Components\TextInput::make('title')
+                            ->live(onBlur: true)
+                            ->required()
+                            ->afterStateUpdated(function (Get $get, Set $set, ?string $old, ?string $state) {
+                                if (($get('slug') ?? '') !== Str::slug($old)) {
+                                    return;
+                                }
+
+                                $set('slug', Str::slug($state));
+                            }),
+//                    ->afterStateUpdated(fn(Set $set, $state) => $set('slug', Str::slug($state)))
+                        Forms\Components\TextInput::make('slug')
+                            ->unique(ignoreRecord: true)
+//                    ->dehydrated()
+                            ->required(),
+                        Forms\Components\Select::make('department_id')
+                            ->label(__('Department'))
+                            ->relationship('department', 'name')
+                            ->required()
+                            ->preload()
+                            ->searchable()
+                            ->reactive()
+                            ->afterStateUpdated(fn(Set $set) => $set('category_id', null)),
+
+                        Forms\Components\Select::make('category_id')
+                            ->label(__('Category'))
+                            ->relationship(
+                                name: 'category',
+                                titleAttribute: 'name',
+                                modifyQueryUsing: fn(Get $get, Builder $query) => $query->where('department_id', $get('department_id'))
+                            )
+//                                modifyQueryUsing: function (Builder $query, callable $get) {
+//                                    // Modify the category query based on the selected department
+//                                    $departmentId = $get('department_id'); // Get selected department
+//                                    if ($departmentId) {
+//                                        $query->where('department_id', $departmentId);
+//                                    }
+//                                }
+//                            )
+                            ->required()
+                            ->preload()
+                            ->searchable()
+                    ]),
+
+                Forms\Components\RichEditor::make('description')
+                    ->label(__('Description'))
+                    ->required()
+                    ->toolbarButtons([
+                        'blockquote',
+                        'bold',
+                        'bulletList',
+                        'h2',
+                        'h3',
+                        'italic',
+                        'link',
+                        'orderedList',
+                        'redo',
+                        'strike',
+                        'underline',
+                        'undo',
+                    ])
+                    ->columnSpan(2),
+
+                Forms\Components\TextInput::make('price')
+                    ->required()
+                    ->numeric(),
+                Forms\Components\TextInput::make('quantity')
+                    ->integer(),
+                Forms\Components\Select::make('status')
+                    ->options(ProductStatusEnum::labels())
+                    ->default(ProductStatusEnum::Draft->value)
+                    ->required()
+            ]);
+    }
+
+    public static function table(Table $table): Table
+    {
+        return $table
+            ->columns([
+                Tables\Columns\TextColumn::make('title')
+                    ->sortable()
+                    ->words(10)
+                    ->searchable(),
+                Tables\Columns\TextColumn::make('status')
+                    ->badge()
+                    ->colors(ProductStatusEnum::colors()),
+                Tables\Columns\TextColumn::make('department.name')
+                    ->sortable()
+                    ->searchable(),
+                Tables\Columns\TextColumn::make('category.name')
+                    ->sortable()
+                    ->searchable(),
+                Tables\Columns\TextColumn::make('created_at')
+                    ->dateTime()
+                    ->sortable()
+                    ->searchable()
+                    ->toggleable(isToggledHiddenByDefault: true),
+                Tables\Columns\TextColumn::make('updated_at')
+                    ->dateTime()
+                    ->sortable()
+                    ->searchable()
+                    ->toggleable(isToggledHiddenByDefault: true),
+            ])
+            ->filters([
+                Tables\Filters\SelectFilter::make('status')
+                    ->options(ProductStatusEnum::labels()),
+                Tables\Filters\SelectFilter::make('department_id')
+                    ->relationship('department', 'name')
+            ])
+            ->actions([
+                Tables\Actions\ViewAction::make(),
+                Tables\Actions\EditAction::make(),
+            ])
+            ->bulkActions([
+                Tables\Actions\BulkActionGroup::make([
+                    Tables\Actions\DeleteBulkAction::make(),
+                ]),
+            ]);
+    }
+
+    public static function getRelations(): array
+    {
+        return [
+            //
+        ];
+    }
+
+    public static function getPages(): array
+    {
+        return [
+            'index' => Pages\ListProducts::route('/'),
+            'create' => Pages\CreateProduct::route('/create'),
+            'view' => Pages\ViewProduct::route('/{record}'),
+            'edit' => Pages\EditProduct::route('/{record}/edit'),
+        ];
+    }
+
+    public static function canViewAny(): bool
+    {
+        /** @var User $user */
+        $user = Filament::auth()->user();
+        return $user && $user->hasRole(RolesEnum::Vendor->value);
+    }
+}
---
+++ app/Models/Product.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\SoftDeletes;
+use Illuminate\Database\Eloquent\Builder;
+
+/**
+ * @mixin Builder
+ */
+
+class Product extends Model
+{
+    /** @use HasFactory<\Database\Factories\ProductFactory> */
+    use HasFactory, SoftDeletes;
+
+    public function department(): BelongsTo
+    {
+        return $this->belongsTo(Department::class);
+    }
+
+    public function category(): BelongsTo
+    {
+        return $this->belongsTo(Category::class);
+    }
+}
