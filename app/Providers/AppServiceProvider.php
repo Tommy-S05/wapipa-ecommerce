8a15aa01086c398d184f8d5c53626723c7253f69
Tommy Sanchez <tommy-s05@hotmail.com>
2025-01-25 19:31:12 -0400

    Lang switcher in filament

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,6 +2,7 @@
 
 namespace App\Providers;
 
+use BezhanSalleh\FilamentLanguageSwitch\LanguageSwitch;
 use Illuminate\Support\Facades\Vite;
 use Illuminate\Support\ServiceProvider;
 
@@ -21,5 +22,11 @@ class AppServiceProvider extends ServiceProvider
     public function boot(): void
     {
         Vite::prefetch(concurrency: 3);
+
+        LanguageSwitch::configureUsing(function(LanguageSwitch $switch) {
+            $switch
+                ->locales(['es', 'en'])
+                ->circular();
+        });
     }
 }
